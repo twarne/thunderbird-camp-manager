a045013da795e87ff3b0bf70d72636d639ef7fa7
Tom Warne <twarne@gmail.com>
2019-04-09 19:54:22 -0700

    refactor(ParticipantDetails): Move participant details pane to separate component

--- src/components/Leaders/index.js
+++ src/components/Leaders/index.js
@@ -28,6 +28,7 @@ import {
 } from '@material-ui/core';
 import moment from 'moment';
 import NavHeader from '../NavHeader';
+import ParticipantDetails from '../ParticipantDetails';
 
 const LeadersPage = props => {
   const [loading, setLoading] = useState(true);
@@ -151,77 +152,7 @@ const LeadersPage = props => {
     return (
       <TableRow>
         <TableCell colSpan={100}>
-          <Grid container spacing={24}>
-            <Grid item xs={6}>
-              <Typography variant="h6" gutterBottom>
-                {form.name}
-              </Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography variant="caption">Birthdate</Typography>
-              <Typography variant="body1">{form.fullData.participant.dateOfBirth}</Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography variant="caption">Allergies</Typography>
-              <Typography variant="body1">
-                {form.hasAllergies ? form.fullData.medicalInformation.allergies : 'None'}
-              </Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography variant="caption">Dietary Restrictions</Typography>
-              <Typography variant="body1">
-                {form.hasDietaryRestrictions ? form.fullData.medicalInformation.dietaryRestrictions : 'None'}
-              </Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography variant="caption">Physical Restrictions</Typography>
-              <Typography variant="body1">
-                {form.hasPhysicalRestrictions ? form.fullData.physicalConditions.physicalRestrictions : 'None'}
-              </Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography variant="caption">Other Accomodations</Typography>
-              <Typography variant="body1">
-                {form.fullData.otherAccomodations.otherConsiderations
-                  ? form.fullData.otherAccomodations.otherConsiderations
-                  : 'None'}
-              </Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    name="paid"
-                    checked={form.paid ? true : false}
-                    onChange={handleCheckboxUpdate(form, 'paid')}
-                    value={form.paid ? 'yes' : 'no'}
-                  />
-                }
-                label={'Paid?'}
-              />
-            </Grid>
-            {/** 
-            <Grid item xs={6}>
-              <TextField
-                id="participantNotes"
-                name="participantNotes"
-                label="Notes"
-                fullWidth
-                multiline
-                autoComplete="notes"
-                helperText="Notes"
-                onChange={handleUpdate(form, 'notes')}
-                value={form.fullData.notes || ''}
-                InputLabelProps={{ shrink: form.fullData.notes ? true : false }}
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <Button onClick={handleSave} disabled={detailNeedsSave}>
-                Save
-              </Button>
-            </Grid>
-            */}
-          </Grid>
+          <ParticipantDetails form={form} onPaidChange={handleCheckboxUpdate(form, 'paid')} />
         </TableCell>
       </TableRow>
     );
